e3b891a2ffa1656992ec22c878586cbe868674a4
Stella <stellax212@gmail.com>
2020-11-05 13:20:17 +0100

    New

--- src/js/dataTable.js
+++ src/js/dataTable.js
@@ -3,28 +3,36 @@ $('#dataFilter').DataTable({
     responsive: true,
     "pageLength": 25,
     "lengthMenu": [25, 50, 100],
+    orderCellsTop: true,
     fixedHeader: {
         header: true
     },
-    "bSort": false,
     initComplete: function () {
-        this.api().columns().every(function () {
-            var column = this;
 
-            var select = $('<select id="formfilter" class="filterdropdown"><option value="">' + $(column.header()).text() + '</option></select>')
-                .appendTo($(column.header()).empty())
-                .on('change', function () {
-                    var val = $.fn.dataTable.util.escapeRegex(
-                        $(this).val()
-                    );
-                    column
-                        .search(val ? '^' + val + '$' : '', true, false)
-                        .draw();
+   
+        $('#dataFilter thead').append('<tr class="c-mainTable__filters"></tr>');
+
+            this.api().columns().every(function () {
+                var column = this;
+                var select = $('<select><option value="">Show all</option></select>')
+                    .appendTo(('#dataFilter .c-mainTable__filters'))
+                    .on('change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search(val ? '^' + val + '$' : '', true, false)
+                            .draw();
+                    });
+
+                column.data().unique().sort().each(function (d, j) {
+                    select.append('<option value="' + d + '">' + d + '</option>')
                 });
 
-            column.data().unique().sort().each(function (d, j) {
-                select.append('<option value="' + d + '">' + d + '</option>')
+                $(this).html('<input type="text" />').appendTo.select;
             });
-        });
+
     }
+
  });
